ee26669dc596d72dfd90059a5767f8f06da975e0
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2020-10-30 18:34:26 +0300

    Add files via upload

---
+++ örnek9.cpp
@@ -0,0 +1,23 @@
+//Rümeysa Coþkun
+//Program amacý:Girilen iki sayýnýn toplamýn alan program.
+#include<iostream>
+using namespace std;
+int main()
+{
+int x,y,sum;
+cout<<"Please enter the first number:";
+cout<<endl;
+cin>>x;
+cout<<endl;
+cout<<"Please enter the second number:";
+cout<<endl;
+cin>>y;
+cout<<endl;
+cout<<"Sum of two numbers:";
+cout<<endl;
+sum=x+y;
+cout<<sum;
+cout<<endl;
+system("PAUSE");
+return 0;	
+}
